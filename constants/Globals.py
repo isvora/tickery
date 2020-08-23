57bc64a6ac157d8423b50ccc04d5e196dc88d835
isvora <isvoranandrei25@gmail.com>
2020-08-23 13:23:00 +0300

    Added punctuations and general comments

--- constants/Globals.py
+++ constants/Globals.py
@@ -1,6 +1,16 @@
+# The Subreddit to be scraped
 SUBREDDIT_NAME = "wallstreetbets"
+
+# Number of submissions to take from the subreddit
 NR_OF_SUBMISSIONS = 25
+
+# List of known bot accounts
 BOT_LIST = ["AutoModerator", "ElonMuskAI"]
+
+# Punctuation
+PUNCTUATIONS = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+# Global lists of data
 SUBMISSIONS = []
 COMMENTS = []
 TICKERDATA = []
