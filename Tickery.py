7f6ced754f41fa4223461e2d7e310e5a97e81dff
isvora <isvoranandrei25@gmail.com>
2020-08-23 13:16:45 +0300

    Added method that checks where the ticker was mentioned and adds all the info in the TICKERDATA list

--- Tickery.py
+++ Tickery.py
@@ -1,11 +1,14 @@
 import datetime
+import re
 
 import praw
 
 from constants.Credentials import API
 from constants.Globals import *
+from constants.Tickers import *
 from data import Comment as cmnt
 from data import Submission as sub
+from data import TickerData as td
 
 
 # Get the time at which a submission was created
@@ -23,8 +26,22 @@ def comment_is_valid(comment):
     # If the author name is a bot, ignore it.
     if comment.author.name in BOT_LIST:
         return False
-
     return True
+
+
+# Set a few rules for the submission being valid
+def submission_is_valid(submission):
+    # If the user deleted its post skip it
+    if submission.author is None:
+        return False
+    return True
+
+
+# Search if the ticker exists in the given content
+def has_ticker(content, ticker):
+    if re.search('\\b' + ticker + '\\b', content):
+        return True
+    return False
 
 
 # Gather all the comments in a submission
@@ -43,6 +60,9 @@ def gather_comments(submission):
                                          body=comment.body,
                                          ups=comment.ups))
 
+    # Add the comments to the global COMMENTS list
+    COMMENTS.extend(comments)
+
     # Return the comments list
     return comments
 
@@ -51,7 +71,7 @@ def gather_comments(submission):
 def gather_submissions(subreddit):
     # Parse the specified number of submissions from the subreddit in the hot category
     for submission in subreddit.hot(limit=NR_OF_SUBMISSIONS):
-        if "Discussion" not in submission.title:
+        if "Discussion" not in submission.title and submission_is_valid(submission):
             comments = gather_comments(submission)
 
             # Create a Submission object and add it to the SUBMISSIONS list
@@ -63,6 +83,29 @@ def gather_submissions(subreddit):
                                               title=submission.title,
                                               upvotes=submission.ups,
                                               upvote_ratio=submission.upvote_ratio))
+
+
+# Populate the TICKERDATA list with TickerData objects
+def parse_data():
+    # Loop through all tickers in the TICKERS list
+    for ticker in TICKERS:
+        submissions = []
+        comments = []
+
+        # Loop through all submissions and check if ticker was mentioned in the title or body
+        for submission in SUBMISSIONS:
+            if has_ticker(submission.title, ticker) or has_ticker(submission.selftext, ticker):
+                submissions.append(submission)
+
+        # Loop through all comments and check if ticker was mentioned in the comment
+        for comment in COMMENTS:
+            if has_ticker(comment.body, ticker):
+                comments.append(comment)
+
+        # Append ticker to the TICKERDATA list
+        TICKERDATA.append(td.TickerData(ticker=ticker,
+                                        comments=comments,
+                                        submissions=submissions))
 
 
 class Tickery:
@@ -82,6 +125,9 @@ class Tickery:
 
         # Start gathering submissions
         gather_submissions(subreddit)
+
+        # Start searching for tickers
+        parse_data()
 
 
 if __name__ == "__main__":
