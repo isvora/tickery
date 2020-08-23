8ecd72cee9c5f07c2f825f64e09d56596b96767a
isvora <isvoranandrei25@gmail.com>
2020-08-23 22:48:48 +0300

    First draft implementation of sentiment analysis

--- Analyser.py
+++ Analyser.py
@@ -1,6 +1,8 @@
 import pickle
 
 import jsonpickle
+from nltk.corpus import stopwords
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
 # Load data from json file
@@ -20,6 +22,29 @@ def load_data():
     return ticker_data_list
 
 
+# Remove stop words from the given text
+def remove_stop_words(content_list):
+    final_list = []
+    for word in content_list:
+        if word not in stopwords.words('english'):
+            final_list.append(word)
+    return final_list
+
+
+def sentiment_analysis(text, upvote):
+    score = SentimentIntensityAnalyzer().polarity_scores(text)
+    neg = score['neg']
+    pos = score['pos']
+
+    if upvote == 0:
+        upvote = 1
+
+    if neg > pos:
+        return neg * upvote * -1
+    else:
+        return pos * upvote
+
+
 class Analyser:
 
     @staticmethod
@@ -27,6 +52,23 @@ class Analyser:
         print("Analysing data...")
 
         ticker_data_list = load_data()
+
+        for ticker_data in ticker_data_list:
+            final_score = 0
+
+            for submission in ticker_data.submissions:
+                score = 0
+                if ticker_data.ticker in submission.title:
+                    score = sentiment_analysis(submission.title, submission.upvotes)
+                if ticker_data.ticker in submission.selftext:
+                    score = sentiment_analysis(submission.selftext, submission.upvotes)
+                final_score = final_score + score
+
+            for comment in ticker_data.comments:
+                score = sentiment_analysis(comment.body, comment.ups)
+                final_score = final_score + score
+
+            print(ticker_data.ticker + " : " + str(final_score))
 
 
 if __name__ == "__main__":
